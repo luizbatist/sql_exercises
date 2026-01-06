ea0e742d0251d2e7693c4fafa3f9bfe4ff57605d
Luiz Batista <luizhnr2004@gmail.com>
2026-01-06 11:28:39 -0300

    Add files via upload

--- Semana 01/Modulo_05/Where-pt2.sql
+++ Semana 01/Modulo_05/Where-pt2.sql
@@ -4,3 +4,14 @@ WHERE BrandName = 'Fabrikam'
 
 SELECT * FROM DimProduct
 WHERE ColorName = 'Black'
+
+-- USANDO OPERADORES LÓGICOS AND, OR, NOT
+
+SELECT * FROM DimProduct
+WHERE BrandName = 'Fabrikam' AND ColorName = 'Black'
+
+SELECT * FROM DimProduct
+WHERE BrandName = 'Contoso' OR ColorName = 'White'
+
+SELECT * FROM DimEmployee
+WHERE NOT DepartmentName = 'Marketing'
